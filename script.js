7e2eaa74a2773972fd9d986350b37c5034f421e3
Aryan Namdev <aryanamdev08@gmail.com>
2022-11-08 16:02:02 +0530

    added styles

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let qrValue = document.querySelector("input");
 let button = document.querySelector("button");
+let qrWrapper = document.querySelector(".qr-wrapper");
 let qr = document.querySelector(".qr-wrapper img");
 
 button.addEventListener("click", () => {
   let qrText = qrValue.value;
   console.log(qrText);
   qr.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText}`;
+  qrWrapper.classList.add("active");
+  if (qrValue.innerText.length == 0) {
+    return;
+  }
 });
